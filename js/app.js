28cccdf7b378c9d653a18b47c54b2ef7fd94b794
lloydaarn <lloydaaron.barnacha@yahoo.com>
2023-07-03 15:36:09 +0800

    update scss structure

--- js/app.js
+++ js/app.js
@@ -15,50 +15,12 @@ app.windowSize = function () {
     };
 };
 
-// scroll to top button
-var _scrollHelper = function () {
-    var scrollTopBtnFlag = true;
-    var $scrollTopBtn = $(".scroll-top-btn");
-
-    $scrollTopBtn.click(function () {
-        app.$root.animate(
-            {
-                scrollTop: 0,
-            },
-            1000
-        );
-        return false;
-    });
-
-    $(window).scroll(function () {
-        // reveal scroll top button
-        if ($(this).scrollTop() > 500) {
-            if (scrollTopBtnFlag) {
-                $scrollTopBtn.addClass("reveal-btn");
-                scrollTopBtnFlag = false;
-            }
-        } else {
-            if (!scrollTopBtnFlag) {
-                $scrollTopBtn.removeClass("reveal-btn");
-                scrollTopBtnFlag = true;
-            }
-        }
-
-        if ($(window).scrollTop() + $(window).height() == $(document).height()) {
-            $scrollTopBtn.removeClass("reveal-btn");
-            scrollTopBtnFlag = true;
-        }
-    });
-};
 
 $(document).ready(function () {
-    _scrollHelper();
 
     // plugins init
     AOS.init({
         once: true,
         duration: 500,
     });
-    cssVars();
-    objectFitImages();
 });
